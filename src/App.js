02cd800a83163a84d28060123d56771458e43069
Greg <gmerrill03@gmail.com>
2019-12-13 13:53:10 -0800

    Change footer aos animations

--- src/App.js
+++ src/App.js
@@ -320,14 +320,14 @@ class App extends React.Component {
     <h2 className="w3-text-light-grey">Contact Me</h2>
     <hr style={{ width: '200px' }} className="w3-opacity" />
 
-    <div className="w3-section" data-aos="fade-up" data-aos-anchor-placement="bottom">
+    <div className="w3-section" data-aos="fade-up" data-aos-anchor-placement="bottom-bottom">
       <p><i className="fa fa-map-marker fa-fw w3-text-white w3-xxlarge w3-margin-right"></i> San Francisco, US</p>
       <p><i className="fa fa-phone fa-fw w3-text-white w3-xxlarge w3-margin-right"></i> Phone: &nbsp; 510 210 7036</p>
       <p><i className="fa fa-envelope fa-fw w3-text-white w3-xxlarge w3-margin-right"> </i> Email: &nbsp; gmerrill03@gmail.com</p>
     </div>
     <br/>
-    <p data-aos="fade-up" data-aos-anchor-placement="bottom" data-aos-delay="300">Lets get in touch. Send me a message:</p>
-    <form onSubmit={this.handleSubmit} data-aos="fade-up" data-aos-anchor-placement="bottom" data-aos-delay="300">
+    <p data-aos="fade-right" data-aos-anchor-placement="bottom-bottom" data-aos-delay="300">Lets get in touch. Send me a message:</p>
+    <form onSubmit={this.handleSubmit} data-aos="fade-right" data-aos-anchor-placement="top-bottom">
       <p><input className="w3-input w3-padding-16" type="text" placeholder="Name" required id="name" name="name" onChange={this.handleChange} /></p>
       <p><input className="w3-input w3-padding-16" type="text" placeholder="Email" required id="email" name="email" onChange={this.handleChange} /></p>
       <p><input className="w3-input w3-padding-16" type="text" placeholder="Subject" required id="subject" name="subject" onChange={this.handleChange} /></p>
@@ -342,7 +342,7 @@ class App extends React.Component {
   </div>
   
     {/* <!-- Footer --> */}
-  <footer className="w3-content w3-text-grey w3-xlarge" data-aos="fade-up" data-aos-anchor-placement="bottom-bottom">
+  <footer className="w3-content w3-text-grey w3-xlarge" data-aos="fade-right" data-aos-anchor-placement="top-bottom">
     <div className='w3-col footer-link-ctnr'>
       <div className='w3-third w3-center'>
         <a target='_blank' rel="noopener noreferrer" href='https://github.com/g-merrill/'>
