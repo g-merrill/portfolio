69a6b21b29d5097a77390bd8f69eb34c839ddb1a
Greg <gmerrill03@gmail.com>
2019-12-13 18:47:18 -0800

    Replace large photos with compressed versions

--- src/components/Home.js
+++ src/components/Home.js
@@ -5,7 +5,7 @@ const Home = () => (
   <header className="w3-container w3-padding-32 w3-center w3-black focus-off Home" id="home">
     <h1 className="w3-jumbo"><span className="w3-hide-small">I'm</span> Greg Merrill</h1>
     <p>Software Engineer</p>
-    <img src="https://i.imgur.com/UN2oD55.jpg" alt="boy" className="w3-image" style={{ width: '400px' }} />
+    <img src="https://i.imgur.com/KD4R11W.jpg" alt="greg" className="w3-image" style={{ width: '400px' }} />
   </header>
 );
 
--- src/components/NavLarge.js
+++ src/components/NavLarge.js
@@ -5,7 +5,7 @@ const NavLarge = ({ setActiveTab }) => (
   // Icon Bar (Sidebar - hidden on small screens)
   <nav className="w3-sidebar w3-bar-block w3-small w3-hide-small w3-center NavLarge">
     {/* Avatar image in top left corner */}
-    <img src="https://i.imgur.com/Sj59xHV.jpg" alt="smoke" style={{ width: '100%' }} />
+    <img src="https://i.imgur.com/ncJCyQP.jpg" alt="smoke" style={{ width: '100%' }} />
     <a onClick={setActiveTab} href="#home" className="nav-btn web-nav-btn active-tab w3-bar-item w3-button w3-padding-large w3-hover-black">
       <i className="fa fa-home w3-xxlarge"></i>
       <p>HOME</p>
